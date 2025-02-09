fa73b01e102ced47adfcf1c442c53cb2dd47fe49
Hisham Alyas <hishaam6618@gmail.com>
2025-02-09 13:13:33 +0200

    Refactor stream processing to support multiple resolutions with a master playlist and improved input options for FFmpeg

--- api/controller/streamController.js
+++ api/controller/streamController.js
@@ -4,13 +4,13 @@ exports.startStream = async (req, res) => {
   try {
     const { url, id, name, resolution } = req.body;
 
-    if (!url || !id || !name || !resolution) {
+    if (!url || !id || !name) {
       return res.status(400).json({
         error: "URL, stream ID, name, and resolution settings are required",
       });
     }
 
-    const streams = streamService.processStream(url, id, name, resolution);
+    const streams = streamService.processStream(url, id, name);
 
     return res.status(200).json({
       message: "Streams are being processed...",
